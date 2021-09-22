af14aa3acde357ca844626a07584ae17fe47c6db
Joa Chang Hwa <joachy@naver.com>
2021-09-22 11:57:46 +0900

    best-time-to-buy-and-sell-stock1 commit

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -2,7 +2,7 @@ import sys
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         profit = 0
         min_price = sys.maxsize
 
